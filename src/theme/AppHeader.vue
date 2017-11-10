8c8412dd65f00f8c69b9c7e219aa37c13e13cb9c
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-10 18:44:28 +0000

    adding login form

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,15 +1,21 @@
 <template>
 	<nav class="nav has-shadow">
       <div class="container">
-        <a href="/">
+        <router-link to="/" exact>
           <img src="http://bit.ly/vue-img"
           alt="Vue SPA" />
-        </a>
+        </router-link>
+        <router-link to="/" exact class="nav-item is-tab">
+          Home
+        </router-link>
+        <router-link to="/login" class="nav-item is-tab">
+          Login
+        </router-link>
       </div>
     </nav>
 </template>
 <script>
 	export default {
-		
+
 	}
 </script>
