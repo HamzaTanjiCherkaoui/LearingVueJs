d01ae2325a0837d7f438ba06ce300b5d866d00d4
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-10 18:31:26 +0000

    adding basic routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import AppLayout from './theme/layout.vue'
-
+import router from './router'
 const app = new Vue({
+	router,
   ...AppLayout
 })
 
-export {app}
+export {app , router}
