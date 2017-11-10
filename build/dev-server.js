d2d22ed6518967c8b5e8435edfbffdccc350b741
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-10 15:04:34 +0000

    adding hot reloading

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,16 @@
 const webpack = require("webpack");
-const clientConfig = require("./webpack.base.config.js");
+const clientConfig = require("./webpack.client.config.js");
 
 module.exports = function setupDevServer(app) {
+	clientConfig.entry.app  = [
+		"webpack-hot-middleware/client",
+		clientConfig.entry.app
+	];
+	
+	clientConfig.plugins.push(
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+		);
 	const clientCompiler = webpack(clientConfig);
 	app.use(
 		require("webpack-dev-middleware")(clientCompiler,{
--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 
 const app = new Vue({
 		data : {
-			hello : "hey there"
-		}
+			hello : "hey there 8"
+		}, 
+		template :`<div id ="app"> {{ hello }}  </div>`
 });
 
 export {app};
