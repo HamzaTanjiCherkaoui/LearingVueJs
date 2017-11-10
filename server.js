f5b331d779bc7d45ab5d3f19ebaad8d16ce83233
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-11-10 14:05:31 +0000

    rendring index.html

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
+const path = require("path");
 
 const port = process.env.port || 3000;
 
+const indexHTML = (()=> {
+	return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+})();
+
+
 
 app.get("*",(req,res) => {
-res.write("hello world !");
+res.write(indexHTML);
 res.end();
 })
 
